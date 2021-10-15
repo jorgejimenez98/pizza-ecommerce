9c1f4234438859fd2480cff2f400959a70e0a911
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 01:36:20 +0200

    INIT browser router on app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import NavBar from "./components/NavBar";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import ScrollTop from "./components/ScrollTop";
+import Login from "./views/Login";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      <ScrollTop />
+      <Switch>
+        <Route exact path="/login" render={(props) => <Login {...props} />} />
+        <Route path="/" render={(props) => <MainLayout {...props} />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
