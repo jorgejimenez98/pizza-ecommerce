4d8e27df642d77520d5830df95e45c9ce58700f7
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 01:35:33 +0200

    CREATE mainlayout component /

---
+++ client/src/layouts/MainLayout.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { routes } from "../routes";
+import NavBar from "../components/NavBar";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+function MainLayout() {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        {routes.map((route, idx) => {
+          return (
+            route.component && (
+              <Route
+                key={idx}
+                path={route.path}
+                exact={route.exact}
+                render={(props) => <route.component {...props} />}
+              />
+            )
+          );
+        })}
+        <Redirect from="/" to="/home" />
+      </Switch>
+    </React.Fragment>
+  );
+}
+
+export default MainLayout;
