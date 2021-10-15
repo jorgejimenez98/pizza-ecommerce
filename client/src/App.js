3fa1d694e94fd481f626f28c73691afad35fbc58
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 00:52:52 +0200

    INIT app component with header

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap'
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-       <div className="container">
-          <div className="card">
-            <h1 className="text-dark">Hola mundo</h1>
-            <Button variant='primary' size={'xl'}>Boton</Button>
-          </div>
-       </div>
-
-      </header>
+    <div>
+      <NavBar />
     </div>
   );
 }
