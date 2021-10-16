a80ec9f94a5e6e1dbf38301b819628039006766b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 03:07:28 +0200

    CREATE image dialog component

---
+++ client/src/components/ImageModal.jsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import { CardMedia } from "@mui/material";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
+
+const BootstrapDialogTitle = (props) => {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default function ImageModal({ image, title, open, handleClose }) {
+  return (
+    <div>
+      <BootstrapDialog onClose={handleClose} open={open}>
+        <BootstrapDialogTitle onClose={handleClose}>
+          {title}
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <CardMedia
+            component="img"
+            height="400"
+            with="400"
+            image={image}
+            alt="pizza"
+          />
+        </DialogContent>
+      </BootstrapDialog>
+    </div>
+  );
+}
