bded52a2786277cae2107a3f6d9f21990e9075ed
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 05:09:26 +0200

    ADD /getPizzas route to server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+
+// Models
+const Pizzas = require("./models/pizzaModel");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Server Working!"));
+app.get("/", (req, res) => res.send(`Server Working!`));
+
+// PIZZA ROUTES
+app.get("/getPizzas", (req, res) => {
+  Pizzas.find({}, (error, docs) => {
+    if (error) {
+      console.log(`Error loading pizzas ${error}`);
+    } else {
+      res.send(docs);
+    }
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
