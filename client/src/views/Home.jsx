9ec1ec051f8f90c7776f4115d18b35a9e95080ac
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 03:08:05 +0200

    ADD pizzass list to component

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { pizzas } from "../localPizzas";
+import PizzaCard from "../components/PizzaCard";
 
 function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  return (
+    <div className="container text-center">
+      <div className="row">
+        {pizzas.map((pizza, ind) => (
+          <div className="col-md-4" key={ind}>
+            <PizzaCard pizza={pizza} />;
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
