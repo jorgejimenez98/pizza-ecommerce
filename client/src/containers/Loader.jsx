b6def038d76853af77b4f4ea921bfa0c5d1fd831
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 06:00:17 +0200

    CREATE Loader Component

---
+++ client/src/containers/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+function Loader() {
+  return (
+    <div className="text-center">
+      <CircularProgress />
+    </div>
+  );
+}
+
+export default Loader;
