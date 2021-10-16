38d1ee549aa6dcbb44a1248a3ed283fd742e4b7e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 06:01:07 +0200

    CREATE containers index

---
+++ client/src/containers/index.js
@@ -0,0 +1,6 @@
+import Loader from "./Loader";
+import Message from "./Message";
+import NavBar from "./NavBar";
+import ScrollTop from "./ScrollTop";
+
+export { ScrollTop, Loader, Message, NavBar };
