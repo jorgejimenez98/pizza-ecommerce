e5eae5f4c977aefeb83b445992bf8afcc0d1dbd8
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-16 06:04:03 +0200

    ADD, get list from redux store

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,17 +1,39 @@
-import React from "react";
-import { pizzas } from "../localPizzas";
-import PizzaCard from "../components/PizzaCard";
+import React, { useEffect } from "react";
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+import { getPizzasList } from "../redux/actions/pizzas.actions";
+// Components
+import { PizzaCard } from "../components";
+import { Loader, Message } from "../containers";
 
 function Home() {
+  const dispatch = useDispatch();
+
+  const {
+    loading: loadingList,
+    error: errorList,
+    pizzas,
+  } = useSelector((state) => state.pizzas.list);
+
+  useEffect(() => {
+    dispatch(getPizzasList());
+  }, [dispatch]);
+
   return (
     <div className="container text-center">
-      <div className="row">
-        {pizzas.map((pizza, ind) => (
-          <div className="col-md-4" key={ind}>
-            <PizzaCard pizza={pizza} />;
-          </div>
-        ))}
-      </div>
+      {loadingList ? (
+        <Loader />
+      ) : errorList ? (
+        <Message message={errorList} type="error" />
+      ) : (
+        <div className="row">
+          {pizzas.map((pizza, ind) => (
+            <div className="col-md-4" key={ind}>
+              <PizzaCard pizza={pizza} />;
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
