b5478a295cef2709c7a9fc44c429c43d47e400fb
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 03:16:07 +0200

    ADD materiall list to component

--- client/src/components/cart/CartItemsList.jsx
+++ client/src/components/cart/CartItemsList.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import * as React from "react";
+import {
+  List,
+  ListItem,
+  Divider,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Typography,
+  ListItemSecondaryAction,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
 
-function CartItemsList() {
-    return (
-        <div>
-            CartItemsList
-        </div>
-    )
+import { Delete } from "@material-ui/icons";
+
+export default function CartItemsList() {
+  return (
+    <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Ali Connors
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+        />
+        <ListItemSecondaryAction>
+          <Tooltip title="Delete">
+            <IconButton edge="end">
+              <Delete />
+            </IconButton>
+          </Tooltip>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+    </List>
+  );
 }
-
-export default CartItemsList
