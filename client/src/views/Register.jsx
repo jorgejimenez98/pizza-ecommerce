a6cd6bafbc18e96b7da962d8c483d4f5ef9e4c39
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 06:26:40 +0200

    ADD validations to register form

--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -1,10 +1,30 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { Form } from "react-bootstrap";
-import { Avatar, Button, CssBaseline, Box } from "@mui/material";
+import { Avatar, Button, CssBaseline } from "@mui/material";
 import LocalPizzaOutlinedIcon from "@material-ui/icons/LocalPizzaOutlined";
+// Formik
+import { useFormik } from "formik";
+import {
+  initialRegisterValues,
+  registerSchema,
+} from "../core/formik-validations";
+// Form Controls
+import {
+  EmailAdressFormControl,
+  NameFormControl,
+  PasswordWithValidationFormControl,
+  ConfirmPasswordFormControl,
+} from "../core/form-controls";
 
 export default function Register() {
+  const formik = useFormik({
+    initialValues: initialRegisterValues,
+    validationSchema: registerSchema,
+    onSubmit: (values) => {
+      console.log("Register", values);
+    },
+  });
+
   return (
     <div className="register center-form">
       <CssBaseline />
@@ -14,28 +34,11 @@ export default function Register() {
             <LocalPizzaOutlinedIcon />
           </Avatar>
           <h3>Register</h3>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Label className="float-left">Name</Form.Label>
-                <Form.Control type="text" placeholder="Name" />
-              </Form.Group>
-
-              <Form.Group className="mb-3">
-                <Form.Label className="float-left">Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-
-              <Form.Group className="mb-3">
-                <Form.Label className="float-left">Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-
-              <Form.Group className="mb-3">
-                <Form.Label className="float-left">Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm Password" />
-              </Form.Group>
-            </Form>
+          <form onSubmit={formik.handleSubmit}>
+            <NameFormControl formik={formik} />
+            <EmailAdressFormControl formik={formik} />
+            <PasswordWithValidationFormControl formik={formik} />
+            <ConfirmPasswordFormControl formik={formik} />
             <Button
               type="submit"
               fullWidth
@@ -49,7 +52,7 @@ export default function Register() {
                 {"Already have an account? Sign In"}
               </Link>
             </div>
-          </Box>
+          </form>
         </div>
       </div>
     </div>
