370f1d42756994bcb1a1fb7b47df31a495fd28b8
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 06:11:31 +0200

    CREATE formik validations index

---
+++ client/src/core/formik-validations/index.js
@@ -0,0 +1,9 @@
+import { initialLoginValues, loginSchema } from "./loginchema";
+import { registerSchema, initialRegisterValues } from "./register-schema";
+
+export {
+  initialLoginValues,
+  initialRegisterValues,
+  loginSchema,
+  registerSchema,
+};
