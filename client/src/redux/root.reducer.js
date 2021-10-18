81a57e50c4dbf7ef7d9b14253412d6a1a8df3323
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-19 01:56:08 +0200

    ADD user and snackbar reducers to main reducers

--- client/src/redux/root.reducer.js
+++ client/src/redux/root.reducer.js
@@ -3,8 +3,12 @@ import { combineReducers } from "redux";
 // Reducers
 import pizzasReducers from "./reducers/pizzas.reducers";
 import cartReducers from "./reducers/cart.reducers";
+import userRedicers from "./reducers/user.reducers";
+import snackbarReducer from "./reducers/snackbar.reducers";
 
 export default combineReducers({
   pizzas: pizzasReducers,
   cart: cartReducers,
+  users: userRedicers,
+  snackbar: snackbarReducer,
 });
