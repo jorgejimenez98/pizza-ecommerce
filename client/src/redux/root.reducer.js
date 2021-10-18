755d21d34d0b97cbc6904ee1c50c7d66e0202f73
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 02:38:07 +0200

    ADD cart reducers to combine reducers

--- client/src/redux/root.reducer.js
+++ client/src/redux/root.reducer.js
@@ -2,7 +2,9 @@ import { combineReducers } from "redux";
 
 // Reducers
 import pizzasReducers from "./reducers/pizzas.reducers";
+import cartReducers from "./reducers/cart.reducers";
 
 export default combineReducers({
   pizzas: pizzasReducers,
+  cart: cartReducers,
 });
