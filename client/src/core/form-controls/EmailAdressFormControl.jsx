a908bd2f3b039cc25ed5b5a4a229d5baa761f70b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 06:10:57 +0200

    CREATE email form control component

---
+++ client/src/core/form-controls/EmailAdressFormControl.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+function EmailAdressFormControl({ formik }) {
+  return (
+    <Form.Group className="mb-3">
+      <Form.Label className="float-left">Email address</Form.Label>
+      <Form.Control
+        type="email"
+        placeholder="Enter email"
+        name="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        isInvalid={formik.touched.email && Boolean(formik.errors.email)}
+        isValid={formik.touched.email && !Boolean(formik.errors.email)}
+      />
+      {formik.touched.email && Boolean(formik.errors.email) && (
+        <Form.Control.Feedback type="invalid">
+          <span className="float-left">{formik.errors.email}</span>
+        </Form.Control.Feedback>
+      )}
+    </Form.Group>
+  );
+}
+
+export default EmailAdressFormControl;
