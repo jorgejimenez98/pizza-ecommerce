080a0374444267a040a13e7588b0ffeb2f902c2f
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-19 01:56:28 +0200

    ADD snackbar to component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
-import { ScrollTop } from "./containers";
+import { ScrollTop, CustomizedSnackbars } from "./containers";
 import { Login, Register } from "./views";
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollTop />
+      <CustomizedSnackbars /> 
       <Switch>
         <Route exact path="/login" render={(props) => <Login {...props} />} />
         <Route exact path="/register" render={(props) => <Register {...props} />} />
