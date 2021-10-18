ef23c2434952def5c4d6e08cb7c6945187ae2e49
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 06:26:27 +0200

    ADD pass, name, confirm components

--- client/src/core/form-controls/index.js
+++ client/src/core/form-controls/index.js
@@ -1,4 +1,13 @@
 import EmailAdressFormControl from "./EmailAdressFormControl";
 import PasswordFormControl from "./PasswordFormControl";
+import NameFormControl from "./NameFormControl";
+import PasswordWithValidationFormControl from "./PasswordWithValidationFormControl";
+import ConfirmPasswordFormControl from "./ConfirmPasswordFormControl";
 
-export { EmailAdressFormControl, PasswordFormControl };
+export {
+  EmailAdressFormControl,
+  PasswordFormControl,
+  NameFormControl,
+  PasswordWithValidationFormControl,
+  ConfirmPasswordFormControl,
+};
