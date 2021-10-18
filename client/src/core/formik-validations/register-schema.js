c45a70ccf95082118421d77cbfe63270a575356e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 06:11:51 +0200

    CREATE register formik settings

---
+++ client/src/core/formik-validations/register-schema.js
@@ -0,0 +1,31 @@
+import * as yup from "yup";
+
+export const initialRegisterValues = {
+  name: "",
+  email: "",
+  rol: "",
+  newPassword: "",
+  confirmPassword: "",
+};
+
+export const registerSchema = yup.object({
+  name: yup.string().trim().required("Name is required"),
+  email: yup
+    .string()
+    .trim()
+    .email("Write a valid email. Ex: name@email.com")
+    .required("Email is required"),
+  newPassword: yup
+    .string()
+    .matches(/^(\S+$)/, "Password must not have white spaces")
+    .matches(/[0-9]/, "Password mast have at least a number")
+    .matches(/[a-z]/, "Password mast have at least a lowercase letter")
+    .matches(/[A-Z]/, "Password mast have at least a uppercase letter")
+    .min(6, "Password mast have at least 6 characters")
+    .required("Password is required"),
+  confirmPassword: yup
+    .string()
+    .trim()
+    .oneOf([yup.ref("newPassword"), null], "Passwords do not match")
+    .required("Comfirm Password is required"),
+});
