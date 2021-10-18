3bbf2ec77bd6c3d1c8393735a2deba7cf954b942
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 04:20:22 +0200

    ADD addOrSubstract function and props

--- client/src/views/Cart.jsx
+++ client/src/views/Cart.jsx
@@ -1,18 +1,32 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../redux/actions/cart.actions";
 import { CartItemsList, SubtotalPrice } from "../components";
 
 function Cart() {
+  const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart.cartItems);
 
   const deleteItem = (id) => {
     console.log("Delete Item", id);
   };
 
+  const addOrSubstract = (isAdd, pizza, quantity, varient) => {
+    if (isAdd) {
+      dispatch(addToCart(pizza, quantity + 1, varient));
+    } else if (!isAdd && quantity !== 1) {
+      dispatch(addToCart(pizza, quantity - 1, varient));
+    }
+  };
+
   return (
     <div className="row pl-4 pr-4">
       <div className="col-md-6">
-        <CartItemsList cartItems={cartItems} deleteItem={deleteItem} />
+        <CartItemsList
+          cartItems={cartItems}
+          deleteItem={deleteItem}
+          addOrSubstract={addOrSubstract}
+        />
       </div>
       <div className="col-md-6">
         <SubtotalPrice cartItems={cartItems} />
