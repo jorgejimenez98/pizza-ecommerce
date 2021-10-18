510f7acbd4139d9bf788a93a38bf4fe3b573196e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 03:04:00 +0200

    ADD col rows to cart component

--- client/src/views/Cart.jsx
+++ client/src/views/Cart.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { CartItemsList, SubtotalPrice } from "../components";
 
 function Cart() {
-    return (
-        <div>
-            Cart
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8">
+          <CartItemsList />
         </div>
-    )
+        <div className="col-md-4">
+          <SubtotalPrice />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
