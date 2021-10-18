57d19f18209921df4984373f5917f6d76167e7b6
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 04:05:45 +0200

    ADD subtital price text

--- client/src/components/cart/SubtotalPrice.jsx
+++ client/src/components/cart/SubtotalPrice.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@mui/material";
 
-function SubtotalPrice() {
-    return (
-        <div>
-            SubtotalPrice
-        </div>
-    )
+function SubtotalPrice({ cartItems }) {
+  const subtotalPrice = cartItems
+    .reduce((acc, item) => acc + item.price, 0)
+    .toFixed(2);
+
+  return (
+    <div>
+      <h1 className="text-center text-muted">SubTotal = {subtotalPrice} cup</h1>
+
+      <div className="text-center">
+        <Button variant="contained" color="success">
+          Pay Now
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default SubtotalPrice
+export default SubtotalPrice;
