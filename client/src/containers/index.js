49e24dfc97722eb734fee4e404b80aaf76f1c6d7
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-19 01:56:50 +0200

    ADD snackbar component to index

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -2,5 +2,6 @@ import Loader from "./Loader";
 import Message from "./Message";
 import NavBar from "./NavBar";
 import ScrollTop from "./ScrollTop";
+import CustomizedSnackbars from "./Snackbar";
 
-export { ScrollTop, Loader, Message, NavBar };
+export { ScrollTop, Loader, Message, NavBar, CustomizedSnackbars };
