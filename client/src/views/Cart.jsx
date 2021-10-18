4bd0df989cb839d47656293a23dd8025d836bd34
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 03:35:32 +0200

    ADD cartItems from redux to cartItemsList componet

--- client/src/views/Cart.jsx
+++ client/src/views/Cart.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { CartItemsList, SubtotalPrice } from "../components";
 
 function Cart() {
+  const { cartItems } = useSelector((state) => state.cart.cartItems);
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-8">
-          <CartItemsList />
+          <CartItemsList cartItems={cartItems} />
         </div>
         <div className="col-md-4">
           <SubtotalPrice />
