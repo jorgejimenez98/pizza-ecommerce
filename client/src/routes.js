1476262972c56b32a9f020584377167000c7177d
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 05:06:46 +0200

    ADD register component to main routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,8 @@
-import { Home, Cart, Login } from "./views";
+import { Home, Cart, Login, Register } from "./views";
 
 export const routes = [
   { path: "/home", component: Home, exact: true },
   { path: "/cart", component: Cart },
   { path: "/login", component: Login },
+  { path: "/register", component: Register },
 ];
