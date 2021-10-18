d9d67276c51ab254e5b8499dd6ee682c14419a35
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 03:56:06 +0200

    UPDATE cartItems params

--- client/src/redux/actions/cart.actions.js
+++ client/src/redux/actions/cart.actions.js
@@ -6,8 +6,8 @@ export const addToCart =
       _id: pizza._id,
       name: pizza.name,
       image: pizza.image,
-      varient: pizza.varient,
-      quantity: pizza.quantity,
+      varient: varient,
+      quantity: quantity,
       prices: pizza.prices,
       price: pizza.prices[0][varient] * quantity,
     };
