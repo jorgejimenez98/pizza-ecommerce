b58d9aa1637295a752bf8b1ed8702ca52ee04442
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-18 05:06:37 +0200

    ADD register component to index

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,5 +1,6 @@
 import Cart from "./Cart";
 import Home from "./Home";
 import Login from "./Login";
+import Register from "./Register";
 
-export { Cart, Home, Login };
+export { Cart, Home, Login, Register };
