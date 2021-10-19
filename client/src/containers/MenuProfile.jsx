91a4114ddc77aa777c23926e76f7ee20f60e43fd
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-19 04:15:25 +0200

    CREATE menu profile component

---
+++ client/src/containers/MenuProfile.jsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { BiLogOut, BiUser } from "react-icons/bi";
+
+export default function MenuProfile({ user_login }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        <AccountCircle />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={handleClose}
+          disabled
+          className="justify-content-center"
+        >
+          {user_login["name"]}
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <BiUser size={24} />
+          &nbsp;Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <BiLogOut size={24} />
+          &nbsp;LogOut
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
