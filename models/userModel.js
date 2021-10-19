6e0ea1bfff7e6e161097b750be195bfb76f9e736
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-19 04:17:11 +0200

    ADD unique validator to email field on user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema(
   {
     name: { type: String, require },
-    email: { type: String, require },
+    email: { type: String, require, unique: true },
     password: { type: String, require },
     isAdmin: { type: Boolean, default: false, require },
   },
@@ -11,5 +12,7 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.plugin(uniqueValidator, "This email is already taken.");
 
 module.exports = mongoose.model("users", userSchema);
