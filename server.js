55c8c104e6229ebd9955019370e827bdd660bbc5
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-19 04:15:11 +0200

    ADD express json to server

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const db = require("./db");
 
+app.use(express.json());
+
 // Routes
 app.use("/api/pizzas/", PizzaRoute);
 app.use("/api/users/", UserRouter);
