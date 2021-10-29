a9de1b3ef6ddb07faef97e6a8a667603cc59bc07
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 16:05:36 +0200

    ADD shipping adress modal to index imports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@ import ImageModal from "./ImageModal";
 import PizzaCard from "./PizzaCard";
 import CartItemsList from "./cart/CartItemsList";
 import SubtotalPrice from "./cart/SubtotalPrice";
+import ShippinAdress from "./ShippinAdress";
 
-export { PizzaCard, ImageModal, CartItemsList, SubtotalPrice };
+export { PizzaCard, ImageModal, CartItemsList, SubtotalPrice, ShippinAdress };
