a253561d87f85c5413d2c80f7f1c54108b4342a4
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 16:24:56 +0200

    ADD adress formik validaitons to index

--- client/src/core/formik-validations/index.js
+++ client/src/core/formik-validations/index.js
@@ -1,9 +1,12 @@
 import { initialLoginValues, loginSchema } from "./loginchema";
 import { registerSchema, initialRegisterValues } from "./register-schema";
+import { adressSchema, initialAdressValues } from "./adress-schema";
 
 export {
   initialLoginValues,
   initialRegisterValues,
   loginSchema,
   registerSchema,
+  adressSchema,
+  initialAdressValues,
 };
