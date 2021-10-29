a6b586b80cdcb143e5657394b37cea4d823ff395
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 14:51:13 +0200

    CREATE Order Item model

---
+++ models/order-item.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const orderItemSchema = mongoose.Schema({
+  quantity: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Pizza",
+    required: true,
+  },
+});
+
+orderItemSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+orderItemSchema.set("ToJSON", {
+  virtuals: true,
+});
+
+const OrderItem = mongoose.model("OrderItem", orderItemSchema);
+
+module.exports = OrderItem;
