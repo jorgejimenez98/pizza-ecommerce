48c088f3a8c2e46770874ad56226c7579ab15f49
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 17:03:04 +0200

    ADD order reducers to main reducers

--- client/src/redux/root.reducer.js
+++ client/src/redux/root.reducer.js
@@ -5,10 +5,12 @@ import pizzasReducers from "./reducers/pizzas.reducers";
 import cartReducers from "./reducers/cart.reducers";
 import userRedicers from "./reducers/user.reducers";
 import snackbarReducer from "./reducers/snackbar.reducers";
+import orderReducers from "./reducers/order.reducers";
 
 export default combineReducers({
   pizzas: pizzasReducers,
   cart: cartReducers,
   users: userRedicers,
   snackbar: snackbarReducer,
+  orders: orderReducers,
 });
