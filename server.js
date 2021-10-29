2cdfdf362744f6edefe8d9f20858a03d8551cc7d
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 14:40:50 +0200

    REFORMAT routes, add cors and jwt settings

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const app = express();
 
 // Dependencies
 require("./db");
+const cors = require("cors");
 
 // Middleware
 app.use(require("morgan")("tiny")); // Log API method details
 app.use(express.json());
+app.use(cors());
+app.options("*", cors());
+app.use(require("./helpers/jwt")()); // Protect API Authentication
+app.use(require("./helpers/errorhandler"));
 
 // Routes
-app.use("/api/pizzas/", require("./routes/pizzasRoutes"));
-app.use("/api/users/", require("./routes/userRoutes"));
+app.use("/api/pizzas", require("./routes/pizzasRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => res.send(`Server Working!`));
