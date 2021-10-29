9c9ea02e54388b93975d0b15c62a8355b9959b53
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 16:09:22 +0200

    UPDATE click button hander

--- client/src/components/cart/SubtotalPrice.jsx
+++ client/src/components/cart/SubtotalPrice.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { Button } from "@mui/material";
 
-function SubtotalPrice({ cartItems }) {
-  const subtotalPrice = cartItems
-    .reduce((acc, item) => acc + item.price, 0)
-    .toFixed(2);
-
+function SubtotalPrice({ subtotalPrice, handlePayButtom }) {
   return (
     <div>
       <h1 className="text-center text-muted">SubTotal = {subtotalPrice} cup</h1>
-
       <div className="text-center">
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => {
-            alert(`Make method on paypal to pay ${subtotalPrice} cup`);
-          }}
-        >
+        <Button variant="contained" color="success" onClick={handlePayButtom}>
           Pay Now
         </Button>
       </div>
