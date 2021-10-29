8feb5b97981da09a08e05330a883cedee2ebd1a0
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 16:32:04 +0200

    INIT Form Adress to order

--- client/src/components/ShippinAdress.jsx
+++ client/src/components/ShippinAdress.jsx
@@ -1,26 +1,42 @@
 import React from "react";
+// Formik
+import { useFormik } from "formik";
+import { initialAdressValues, adressSchema } from "../core/formik-validations";
+// Material Mui Components
 import {
   Button,
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
 } from "@mui/material";
 
-function ShippinAdress({ open, handleClose }) {
+function ShippinAdress({ open, handleClose, subtotalPrice }) {
+  const formik = useFormik({
+    initialValues: initialAdressValues,
+    validationSchema: adressSchema,
+    onSubmit: (values) => {
+      console.log("ADD", values);
+    },
+  });
+
   return (
     <Dialog fullWidth maxWidth={"lg"} open={open} onClose={handleClose}>
-      <DialogTitle>Optional sizes</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          You can set my maximum width and whether to adapt or not.
-        </DialogContentText>
-        Texqiu asdasd
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Close</Button>
-      </DialogActions>
+      <form onSubmit={formik.handleSubmit}>
+        <DialogTitle>
+          <div>
+            Insert Shipping Adress{" "}
+            <p className="float-right">{subtotalPrice} cup</p>{" "}
+          </div>
+        </DialogTitle>
+        <DialogContent></DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+          <Button type="submit" variant="contained">
+            Finish Order
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
