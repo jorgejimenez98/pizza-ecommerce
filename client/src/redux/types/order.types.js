0cc871f73a5cc1e5537f66be5ee028f3c1e076e0
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 17:02:29 +0200

    CREATE Order types

---
+++ client/src/redux/types/order.types.js
@@ -0,0 +1,11 @@
+const ADD = {
+    REQUEST: "ORDER_ADD_REQUEST",
+    SUCCESS: "ORDER_ADD_SUCCESS",
+    ERROR: "ORDER_ADD_ERROR",
+    RESET: "ORDER_ADD_RESET",
+  };
+  
+  export const OrderActionTypes = {
+    ADD_ORDER: ADD,
+  };
+  
