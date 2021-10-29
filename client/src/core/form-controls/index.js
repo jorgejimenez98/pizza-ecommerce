e9d38a370314805faf815c9daec073ef4c9fecf4
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 16:35:34 +0200

    ADD ShippingAdress1FormControl component to index

--- client/src/core/form-controls/index.js
+++ client/src/core/form-controls/index.js
@@ -3,6 +3,7 @@ import PasswordFormControl from "./PasswordFormControl";
 import NameFormControl from "./NameFormControl";
 import PasswordWithValidationFormControl from "./PasswordWithValidationFormControl";
 import ConfirmPasswordFormControl from "./ConfirmPasswordFormControl";
+import ShippingAdress1FormControl from "./ShippingAdress1FormControl";
 
 export {
   EmailAdressFormControl,
@@ -10,4 +11,5 @@ export {
   NameFormControl,
   PasswordWithValidationFormControl,
   ConfirmPasswordFormControl,
+  ShippingAdress1FormControl,
 };
