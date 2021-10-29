a33743a80309d2fd986ca2bf6a60e5e8a7b5db8e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 13:33:22 +0200

    ADD user methods to model

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,55 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, require: true },
-    isAdmin: { type: Boolean, default: false, required: true },
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  email: {
+    type: String,
+    required: true,
+    match: [/\S+@\S+\.\S+/, "Email is invalid"],
+    unique: true,
+    index: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-userSchema.plugin(uniqueValidator, "is already taken.");
+UserSchema.plugin(uniqueValidator, { message: "is already taken." });
 
-module.exports = mongoose.model("users", userSchema);
+UserSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+UserSchema.set("toJSON", {
+  virtuals: true,
+});
+
+// Methods
+
+// Check Password
+UserSchema.methods.checkPassword = function (password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+};
+
+// SET Password
+UserSchema.methods.set_passwordHash = function (password) {
+  this.passwordHash = bcrypt.hashSync(password, 10);
+};
+
+// GET USER TO AUTH JSON
+UserSchema.methods.toAuthJson = function () {
+  return { email: this.email, name: this.name, isAdmin: this.isAdmin };
+};
+
+const User = mongoose.model("users", UserSchema);
+module.exports = User;
