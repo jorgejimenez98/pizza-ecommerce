cbbd2d88ee560fa4df88d04389e53a8a119733cb
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-29 17:21:16 +0200

    ADD post route to order routes

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,4 +2,7 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/order.controller");
 
+// POST USER ORDER
+router.post("/", orderController.postUserOrder);
+
 module.exports = router;
