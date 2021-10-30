c4727e18eabe5dc570864139ebd1aa14f2019b46
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-30 16:08:44 +0200

    GET ALL user orders in component

--- client/src/views/UserOrders.jsx
+++ client/src/views/UserOrders.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserOrders } from "../redux/actions/order.actions";
+import { Loader, Message } from "../containers";
 
 function UserOrders() {
-    return (
-        <div>
-            UserOrders
-        </div>
-    )
+  const dispatch = useDispatch();
+
+  // User Info Selector:
+  const { user_login } = useSelector((state) => state.users.login);
+  // Get User Orders Selector
+  const { loading, error, orders } = useSelector(
+    (state) => state.orders.userOrders
+  );
+
+  console.log(orders);
+
+  useEffect(() => {
+    if (user_login) {
+      dispatch(getUserOrders(user_login._id));
+    }
+  }, [user_login, dispatch]);
+
+  return (
+    <React.Fragment>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message type="error" message={error} />
+      ) : (
+        <React.Fragment>All id ok</React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
 
-export default UserOrders
+export default UserOrders;
