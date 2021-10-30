f47335e329a1a7de091c1ed9f9cbaf43e2552688
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-30 15:44:45 +0200

    ADD User Orders Component to routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
-import { Home, Cart } from "./views";
+import { Home, Cart, UserOrders } from "./views";
 
 export const routes = [
   { path: "/home", component: Home, exact: true },
   { path: "/cart", component: Cart },
+  { path: "/my/orders", component: UserOrders },
 ];
