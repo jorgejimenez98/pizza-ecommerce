f5ad0ac90a71de00eded6ef59f0e3133c6ca04c4
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-30 16:08:26 +0200

    CREATE get user orders actions on redux

--- client/src/redux/actions/order.actions.js
+++ client/src/redux/actions/order.actions.js
@@ -22,3 +22,26 @@ export const addUserOrder = (values) => async (dispatch) => {
     });
   }
 };
+
+export const getUserOrders = (userId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: OrderActionTypes.USER_ORDERS_LIST.REQUEST,
+    });
+
+    const { data } = await axios.post(`/api/orders/user/${userId}`);
+
+    dispatch({
+      type: OrderActionTypes.USER_ORDERS_LIST.SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: OrderActionTypes.USER_ORDERS_LIST.ERROR,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
