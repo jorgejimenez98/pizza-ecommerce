e2691258dae5466f2090ab6b309b093de92b1a84
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-30 14:07:30 +0200

    GET data from req.body

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -3,8 +3,13 @@ const Order = require("../models/order.model");
 // Register User
 exports.postUserOrder = async (req, res) => {
   try {
+    // Get Data from req.body
     const data = req.body;
-    console.log(data);
+    const { shippingAddress1, shippingAddress2, city, country, phone } =
+      data.form;
+    const { subtotalPrice, cartItems } = data;
+    console.log(shippingAddress1, shippingAddress2, city, country, phone);
+    console.log(subtotalPrice, cartItems);
     res.send("ALL is OK");
   } catch (error) {
     res.status(404).send({ detail: error.message });
