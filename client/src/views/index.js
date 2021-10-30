bb7fa0f24856fb3cd9b1d6384fe5c416cffe3693
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-30 15:44:34 +0200

    ADD User Orders Component to index

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -2,5 +2,6 @@ import Cart from "./Cart";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
+import UserOrders from "./UserOrders";
 
-export { Cart, Home, Login, Register };
+export { Cart, Home, Login, Register, UserOrders };
