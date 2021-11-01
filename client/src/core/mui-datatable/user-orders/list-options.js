7e9033e9df026b31ccfa82f4fcc0efc8a647ca4f
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-01 14:31:17 +0100

    CREATE list options of user order list

---
+++ client/src/core/mui-datatable/user-orders/list-options.js
@@ -0,0 +1,14 @@
+export const listOptions = {
+  filterType: "textField",
+  responsive: "vertical",
+  selectableRows: "none",
+  searchPlaceholder: "Escribe aquí",
+  rowsPerPage: 10,
+  rowsPerPageOptions: [5, 10, 15, 25, 100],
+  download: false,
+  print: false,
+  viewColumns: false,
+  filter: false,
+};
+
+export default listOptions;
