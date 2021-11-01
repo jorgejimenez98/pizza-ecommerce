d90f3d08027fe4471ca10b44bb63a57b84bceb80
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-01 14:32:04 +0100

    CREATE orderitems columns of user order list

---
+++ client/src/core/mui-datatable/user-orders/OrderItemsColumn.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+function OrderItemsColumn({ orderItems }) {
+  return (
+    <List>
+      {orderItems.map((item, i) => {
+        const totalPrice = item.quantity * item.pizza.prices[0][item.varient];
+        return (
+          <ListItem disablePadding key={i}>
+            <ListItemButton>
+              <ListItemText
+                primary={`${item.pizza.name}: [${item.varient}] * ${item.quantity} = ${totalPrice}`}
+              />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
+
+export default OrderItemsColumn;
