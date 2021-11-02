bf5c1673765539ffc8d275585e5551fcda1ec1fd
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-02 15:55:58 +0100

    ADD Filter Component to index exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,13 @@ import PizzaCard from "./PizzaCard";
 import CartItemsList from "./cart/CartItemsList";
 import SubtotalPrice from "./cart/SubtotalPrice";
 import ShippinAdress from "./ShippinAdress";
+import Filter from "./Filter";
 
-export { PizzaCard, ImageModal, CartItemsList, SubtotalPrice, ShippinAdress };
+export {
+  PizzaCard,
+  ImageModal,
+  CartItemsList,
+  SubtotalPrice,
+  ShippinAdress,
+  Filter,
+};
