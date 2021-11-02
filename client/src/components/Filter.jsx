6248456b4c65967f65ec3e1a68824f0a675663db
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-02 16:09:42 +0100

    INIT Filter design

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Filter() {
-    return (
-        <div>
-            Filter
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("all");
+
+  return (
+    <div className="container mb-3">
+      <div className="row justify-content-center p-3 shadow-lg rounded mt-2">
+        <div className="col-md-4">
+          <div class="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Pizza"
+            />
+          </div>
         </div>
-    )
+        <div className="col-md-4">
+          <div className="form-group">
+            <select
+              className="form-control"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="all">All</option>
+              <option value="veg">Veg</option>
+              <option value="noveg">No Veg</option>
+            </select>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <button className="btn btn-success w-100">Filter</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Filter
+export default Filter;
