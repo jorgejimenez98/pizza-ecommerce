81de963aa406775c4d3319dd4c6e05942bc097c1
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-04 00:11:56 +0100

    ADD user admin login to component

--- client/src/views/AdminPanel.jsx
+++ client/src/views/AdminPanel.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-function AdminPanel() {
-    return (
-        <div>
-            Admin Panel
-        </div>
-    )
+function AdminPanel({ history }) {
+  const { user_login } = useSelector((state) => state.users.login);
+
+  useEffect(() => {
+    if (!user_login) {
+      history.push("/");
+    } else if (!user_login.isAdmin) {
+      history.push("/403");
+    }
+  }, [user_login, history]);
+
+  return <div>Admin Panel</div>;
 }
 
-export default AdminPanel
+export default AdminPanel;
