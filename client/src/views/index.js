c19dc9022abd3adddc580e0ce16c7e59e5a61a99
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-04 00:11:06 +0100

    ADD 403 Error component to index

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -4,5 +4,6 @@ import Login from "./Login";
 import Register from "./Register";
 import UserOrders from "./UserOrders";
 import AdminPanel from "./AdminPanel";
+import Error403 from "./Error403";
 
-export { Cart, Home, Login, Register, UserOrders, AdminPanel };
+export { Cart, Home, Login, Register, UserOrders, AdminPanel, Error403 };
