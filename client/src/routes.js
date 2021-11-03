b4978bd34b5d2c94d52679800aa35834e4da2bb8
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-04 00:11:43 +0100

    ADD 403 Error path to main route

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,9 @@
-import { Home, Cart, UserOrders, AdminPanel } from "./views";
+import { Home, Cart, UserOrders, AdminPanel, Error403 } from "./views";
 
 export const routes = [
   { path: "/home", component: Home, exact: true },
   { path: "/cart", component: Cart },
   { path: "/my/orders", component: UserOrders },
   { path: "/admin/panel", component: AdminPanel },
+  { path: "/403", component: Error403 },
 ];
