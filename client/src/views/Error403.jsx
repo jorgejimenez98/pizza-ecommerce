d1af8d1d5d1926ceedaff25449653e761d21e28b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-04 00:10:50 +0100

    CREATE 403 Error component

---
+++ client/src/views/Error403.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Error403() {
+  return (
+    <div className="center-form">
+      <div className="container text-center">
+        <div className="w-50 m-auto">
+          <h3>Error 403. Access Denied</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Error403;
