c390323b198b0c47f664144e3ed2e51f09bf776c
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-04 00:33:18 +0100

    CREATE admin header component

---
+++ client/src/components/AdminComponents/AdminPanelHeader.jsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import {
+  HiUserGroup,
+  FaPizzaSlice,
+  GrAdd,
+  GrOrderedList,
+} from "react-icons/all";
+import { LinkContainer } from "react-router-bootstrap";
+
+export default function AdminPanelHeader() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="center-form">
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="icon label tabs example"
+        className="bg-white rounded"
+      >
+        <LinkContainer to={"/admin/users/list"}>
+          <Tab icon={<HiUserGroup size={24} />} label="Users List" />
+        </LinkContainer>
+
+        <LinkContainer to={"/admin/pizzas/list"}>
+          <Tab icon={<FaPizzaSlice size={24} />} label="Pizzas List" />
+        </LinkContainer>
+
+        <LinkContainer to={"/admin/pizzas/add"}>
+          <Tab icon={<GrAdd size={24} />} label="Add Pizza" />
+        </LinkContainer>
+
+        <LinkContainer to={"/admin/orders/list"}>
+          <Tab icon={<GrOrderedList size={24} />} label="Orders List" />
+        </LinkContainer>
+      </Tabs>
+    </div>
+  );
+}
