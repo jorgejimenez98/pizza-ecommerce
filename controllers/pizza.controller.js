a37060abac4bc9a0ed2f037e040c72b9ab71e323
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-03 15:30:26 +0100

    ADD filter section to get pizzas list on server

--- controllers/pizza.controller.js
+++ controllers/pizza.controller.js
@@ -2,7 +2,24 @@ const Pizza = require("../models/pizzaModel");
 
 exports.getPizzasList = async (req, res, next) => {
   try {
-    const data = await Pizza.find({});
+    // Filter Section
+    const keyword = req.query.keyword;
+    const cat = req.query.category;
+    let filter = {};
+    if (keyword && cat) {
+      if (cat !== "all") {
+        filter = { category: cat, name: { $regex: keyword.trim() } };
+      } else {
+        filter = { name: { $regex: keyword.trim() } };
+      }
+    } else if (keyword) {
+      filter = { name: { $regex: keyword.trim() } };
+    } else if (cat && cat !== "all") {
+      filter = { category: cat };
+    }
+
+    // GET Data
+    const data = await Pizza.find(filter);
     res.send(data);
   } catch (error) {
     return res.status(400).json({ detail: error });
