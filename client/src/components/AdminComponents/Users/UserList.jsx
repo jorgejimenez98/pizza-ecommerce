f32e856d32513d2dcb6e65f11815d878911218c6
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-05 00:11:18 +0100

    ADD get users list and add settings of user login

--- client/src/components/AdminComponents/Users/UserList.jsx
+++ client/src/components/AdminComponents/Users/UserList.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsersList } from "../../../redux/actions/users.actions";
+import { Loader, Message } from "../../../containers";
 
-function UserList() {
-    return (
-        <div>
-            Users List
-        </div>
-    )
+function UserList({ history }) {
+  const dispatch = useDispatch();
+
+  // User Login Selector
+  const { user_login } = useSelector((state) => state.users.login);
+
+  // Users  List Selector
+  const { loading, error, users } = useSelector((state) => state.users.list);
+
+  console.log("AAAA", users);
+
+  useEffect(() => {
+    if (!user_login) {
+      history.push("/");
+    } else if (!user_login.isAdmin) {
+      history.push("/403");
+    } else {
+      dispatch(getUsersList());
+    }
+  }, [user_login, history, dispatch]);
+
+  return (
+    <React.Fragment>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message message={error} type="error" />
+      ) : (
+        users && (
+          <React.Fragment>
+            <div>Todo is OK</div>
+          </React.Fragment>
+        )
+      )}
+    </React.Fragment>
+  );
 }
 
-export default UserList
+export default UserList;
