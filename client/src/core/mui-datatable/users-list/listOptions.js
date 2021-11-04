58bb8d3584992d2e90dbae4127b6e1ae07684f7a
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-05 00:41:36 +0100

    CREATE user list options for mui datatable

---
+++ client/src/core/mui-datatable/users-list/listOptions.js
@@ -0,0 +1,13 @@
+export const userlistOptions = {
+  filterType: "textField",
+  responsive: "vertical",
+  searchPlaceholder: "Write Here",
+  rowsPerPage: 10,
+  rowsPerPageOptions: [5, 10, 15, 25, 100],
+  download: true,
+  print: false,
+  viewColumns: true,
+  filter: true,
+};
+
+export default userlistOptions;
