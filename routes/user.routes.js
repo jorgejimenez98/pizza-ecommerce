48b2a4984dbf07e1a4e66ac342536ba6ac76686c
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-05 00:09:10 +0100

    ADD get route on users main routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,5 +6,6 @@ const userController = require("../controllers/user.controller");
 router.post("/register", userController.registerUser);
 // LOGIN USER
 router.post("/login", userController.loginUser);
-
+// User List 
+router.get("/", userController.getUserList)
 module.exports = router;
