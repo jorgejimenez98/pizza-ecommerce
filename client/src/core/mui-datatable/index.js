e422bfc33203108d5964220cffd937f68ead9a68
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-05 00:42:16 +0100

    ADD userColumns and userListOptions to index

--- client/src/core/mui-datatable/index.js
+++ client/src/core/mui-datatable/index.js
@@ -1,4 +1,6 @@
 import { columns } from "./user-orders/columns";
 import { listOptions } from "./user-orders/list-options";
+import { userColumns } from "./users-list/columns";
+import { userlistOptions } from "./users-list/listOptions";
 
-export { columns, listOptions };
+export { columns, listOptions, userColumns, userlistOptions };
