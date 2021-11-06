57dede696fc8eb8ca047dd54c4cdec107366746b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-06 14:49:35 +0100

    REMOVE match, and min yup functions

--- client/src/core/formik-validations/pizza-schema.js
+++ client/src/core/formik-validations/pizza-schema.js
@@ -16,25 +16,15 @@ export const pizzaSchema = yup.object({
   priceSmall: yup
     .number()
     .positive("Price Small must be positive")
-    .min(1, "Price Small Cannot be 0")
     .required("Price Small is required"),
   priceMedium: yup
     .number()
     .positive("Price Medium must be positive")
-    .min(1, "Price Medium Cannot be 0")
     .required("Price Medium  is required"),
   priceLarge: yup
     .number()
     .positive("Price Large must be positive")
-    .min(1, "Price Large Cannot be 0")
     .required("Price Large  is required"),
-  imageUrl: yup
-    .string()
-    .trim()
-    .matches(
-      /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
-      "Enter correct image url!"
-    )
-    .required("Image Url is required"),
+  imageUrl: yup.string().trim().required("Image Url is required"),
   description: yup.string().trim().required("Description is required"),
 });
