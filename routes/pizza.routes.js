c7173c1d8af73fe8e9d720bef9306ebf9746b144
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-07 00:12:57 +0100

    ADD delete pizzas routes to routes

--- routes/pizza.routes.js
+++ routes/pizza.routes.js
@@ -4,5 +4,6 @@ const pizzaController = require("../controllers/pizza.controller");
 
 router.get("/getPizzas", pizzaController.getPizzasList);
 router.post("/", pizzaController.registerPizza);
+router.post("/deletePizzas", pizzaController.deletePizzas);
 
 module.exports = router;
