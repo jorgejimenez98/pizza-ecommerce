d8038bdfc91cdd51e2de98865a3434de5cd3ab50
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-06 15:24:03 +0100

    ADD create and save pizza model

--- controllers/pizza.controller.js
+++ controllers/pizza.controller.js
@@ -30,7 +30,22 @@ exports.registerPizza = async (req, res) => {
   try {
     // Get Data
     const data = req.body;
-    console.log(data, "ASAA");
+
+    // Create Pizza
+    let pizza = new Pizza({
+      name: data.name,
+      varients: ["small", "medium", "large"],
+      prices: [[data.priceSmall, data.priceMedium, data.priceLarge]],
+      category: data.category,
+      image: data.imageUrl,
+      description: data.description,
+    });
+
+    // Save Pizza
+    pizza = await pizza.save();
+
+    // Return Response
+    if (!pizza) res.status(400).send("Error to create Pizza");
     res.send("Pizza Add Successfully");
   } catch (error) {
     return res.status(400).json({ detail: error });
