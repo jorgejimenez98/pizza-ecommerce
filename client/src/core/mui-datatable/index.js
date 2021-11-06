cf51349e0889bfb73dc6f5aa765884994d842bd5
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-07 00:09:25 +0100

    ADD pizza columns and options to index

--- client/src/core/mui-datatable/index.js
+++ client/src/core/mui-datatable/index.js
@@ -2,5 +2,14 @@ import { columns } from "./user-orders/columns";
 import { listOptions } from "./user-orders/list-options";
 import { userColumns } from "./users-list/columns";
 import { userlistOptions } from "./users-list/listOptions";
+import { pizzacolumns } from "./pizza-list/pizza-columns";
+import { pizzalistOptions } from "./pizza-list/pizza-listoptions";
 
-export { columns, listOptions, userColumns, userlistOptions };
+export {
+  columns,
+  listOptions,
+  userColumns,
+  userlistOptions,
+  pizzacolumns,
+  pizzalistOptions,
+};
