bb0607e0b22bcdb093c8e7fb7e0b0b3dd47147d9
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-07 00:30:39 +0100

    ADD get pizza details route

--- routes/pizza.routes.js
+++ routes/pizza.routes.js
@@ -6,5 +6,6 @@ router.get("/getPizzas", pizzaController.getPizzasList);
 router.post("/", pizzaController.registerPizza);
 router.post("/deletePizzas", pizzaController.deletePizzas);
 router.put("/:pizzaId", pizzaController.editPizza);
+router.get("/:pizzaId", pizzaController.getPizzaDetails);
 
 module.exports = router;
