841823fd7dfea27573c4cc6563b94536b9fb411e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-07 00:28:11 +0100

    ADD edit pizza rotue

--- routes/pizza.routes.js
+++ routes/pizza.routes.js
@@ -5,5 +5,6 @@ const pizzaController = require("../controllers/pizza.controller");
 router.get("/getPizzas", pizzaController.getPizzasList);
 router.post("/", pizzaController.registerPizza);
 router.post("/deletePizzas", pizzaController.deletePizzas);
+router.put("/:pizzaId", pizzaController.editPizza);
 
 module.exports = router;
