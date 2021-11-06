8992abb55d304c5eacab6ff06299be9f5e1323e5
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-06 14:49:59 +0100

    ADD formik form controls and log submit values

--- client/src/components/AdminComponents/Pizzas/AddPizza.jsx
+++ client/src/components/AdminComponents/Pizzas/AddPizza.jsx
@@ -1,84 +1,86 @@
 import React from "react";
+// Form Control Components
+import {
+  NameFormControl,
+  CategoryFormControl,
+  PriceFormControl,
+  ImageurlFormControl,
+  DescriptionFormControl,
+} from "../../../core/form-controls";
+// Formik
+import { useFormik } from "formik";
+import {
+  initialPizzaValues,
+  pizzaSchema,
+} from "../../../core/formik-validations";
 
 function AddPizza() {
+  const formik = useFormik({
+    initialValues: initialPizzaValues,
+    validationSchema: pizzaSchema,
+    onSubmit: (values) => {
+      console.log("AAAA", values);
+    },
+  });
+
   return (
     <div className="container p-lg-5">
-      <div className="row">
-        <div className="col-md-6">
-          {/* Name */}
-          <div className="form-group">
-            <label>Name</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Write Here"
+      <form onSubmit={formik.handleSubmit}>
+        <div className="row">
+          <div className="col-md-6">
+            {/* Name */}
+            <NameFormControl formik={formik} />
+          </div>
+          <div className="col-md-6">
+            {/* Category */}
+            <CategoryFormControl formik={formik} />
+          </div>
+        </div>
+
+        {/* Prices */}
+        <div className="row">
+          <div className="col-md-4">
+            <PriceFormControl
+              name={"priceSmall"}
+              value={formik.values.priceSmall}
+              onChange={formik.handleChange}
+              touched={formik.touched.priceSmall}
+              errors={formik.errors.priceSmall}
+            />
+          </div>
+          <div className="col-md-4">
+            <PriceFormControl
+              name={"priceMedium"}
+              value={formik.values.priceMedium}
+              onChange={formik.handleChange}
+              touched={formik.touched.priceMedium}
+              errors={formik.errors.priceMedium}
+            />
+          </div>
+          <div className="col-md-4">
+            <PriceFormControl
+              name={"priceLarge"}
+              value={formik.values.priceLarge}
+              onChange={formik.handleChange}
+              touched={formik.touched.priceLarge}
+              errors={formik.errors.priceLarge}
             />
           </div>
         </div>
-        <div className="col-md-6">
-          {/* Category */}
-          <div className="form-group">
-            <label>Category</label>
-            <select className="form-control">
-              <option value="veg">Vegan</option>
-              <option value="nonveg">No Vegan</option>
-            </select>
-          </div>
-        </div>
-      </div>
 
-      {/* Prices */}
-      <div className="row">
-        <div className="col-md-4">
-          <div className="form-group">
-            <label>Price Small Pizza</label>
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Write Here"
-            />
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="form-group">
-            <label>Price Medium Pizza</label>
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Write Here"
-            />
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="form-group">
-            <label>Price Large Pizza</label>
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Write Here"
-            />
-          </div>
-        </div>
-      </div>
+        {/* Image URL */}
+        <ImageurlFormControl formik={formik} />
 
-      {/* Image URL */}
-      <div className="form-group">
-        <label>Image URL</label>
-        <input type="text" className="form-control" placeholder="Write Here" />
-      </div>
+        {/* Description */}
+        <DescriptionFormControl formik={formik} />
 
-      {/* Description */}
-      <div className="form-group">
-        <label>Pizza Description</label>
-        <textarea className="form-control" rows="4" placeholder="Write Here" />
-      </div>
-
-      {/* Buttom */}
-      <div className="text-right">
-        <button type="button" className="btn btn-primary">
-          Submit
-        </button>
-      </div>
+        {/* Buttom */}
+        <div className="text-right">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
