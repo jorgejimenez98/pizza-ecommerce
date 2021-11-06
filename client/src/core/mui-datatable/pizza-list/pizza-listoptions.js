b6c87a586308bd0138952a8c0fedc09ef8c7206a
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-07 00:07:59 +0100

    CREATE pizzas list options

---
+++ client/src/core/mui-datatable/pizza-list/pizza-listoptions.js
@@ -0,0 +1,13 @@
+export const pizzalistOptions = {
+  filterType: "textField",
+  responsive: "vertical",
+  searchPlaceholder: "Write Here",
+  rowsPerPage: 10,
+  rowsPerPageOptions: [5, 10, 15, 25, 100],
+  download: false,
+  print: false,
+  viewColumns: true,
+  filter: true,
+};
+
+export default pizzalistOptions;
