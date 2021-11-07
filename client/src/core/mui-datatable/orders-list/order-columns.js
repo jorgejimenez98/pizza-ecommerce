2a676a0cc6bab05abcfe38ab17f18301ff150e57
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-07 02:03:11 +0100

    CREATE order list columns

---
+++ client/src/core/mui-datatable/orders-list/order-columns.js
@@ -0,0 +1,63 @@
+import UserColumn from "./columns/UserColumn";
+import StatusColumn from "./columns/StatusColumn";
+
+export const orderscolumns = [
+  {
+    name: "_id",
+    label: "Order Id",
+    options: {
+      filter: false,
+      sort: true,
+      display: true,
+      viewColumns: true,
+    },
+  },
+  {
+    name: "user",
+    label: "User",
+    options: {
+      filter: false,
+      sort: false,
+      viewColumns: false,
+      customBodyRender: (value, tableMeta) => {
+        const name = tableMeta.rowData[2];
+        return <UserColumn user={value} />;
+      },
+    },
+  },
+  {
+    name: "totalPrice",
+    label: "Ammount",
+    options: {
+      filter: true,
+      sort: true,
+      viewColumns: true,
+    },
+  },
+  {
+    name: "dateOrdered",
+    label: "Prices",
+    options: {
+      filter: false,
+      sort: false,
+      viewColumns: false,
+      customBodyRender: (value, tableMeta) => {
+        return <div>{value.replace(/T/, " ").replace(/\..+/, "")}</div>;
+      },
+    },
+  },
+  {
+    name: "status",
+    label: "Status",
+    options: {
+      filter: true,
+      filterType: "dropdown",
+      sort: true,
+      viewColumns: true,
+      customBodyRender: (status, tableMeta) => {
+        const orderId = tableMeta.rowData[0];
+        return <StatusColumn status={status} orderId={orderId} />;
+      },
+    },
+  },
+];
