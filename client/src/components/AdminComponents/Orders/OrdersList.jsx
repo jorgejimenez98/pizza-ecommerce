d8c506557812c42fd1e63e3478ac0622bd6a5a34
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-07 02:13:39 +0100

    ADD order lista table design

--- client/src/components/AdminComponents/Orders/OrdersList.jsx
+++ client/src/components/AdminComponents/Orders/OrdersList.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getOrdersList } from "../../../redux/actions/order.actions";
+import { Loader, Message } from "../../../containers";
+import { orderlistOptions, orderscolumns } from "../../../core/mui-datatable";
+import MUIDataTable from "mui-datatables";
 
-function OrdersList() {
-    return (
-        <div>
-            Order List
-        </div>
-    )
+function OrdersList({ history }) {
+  const dispatch = useDispatch();
+
+  // User Info Selector:
+  const { user_login } = useSelector((state) => state.users.login);
+  // Get User Orders Selector
+  const { loading, error, orders } = useSelector(
+    (state) => state.orders.list
+  );
+
+  useEffect(() => {
+    if (!user_login) {
+      history.push("/");
+    } else {
+      dispatch(getOrdersList());
+    }
+  }, [user_login, dispatch, history]);
+
+  return (
+    <React.Fragment>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message type="error" message={error} />
+      ) : (
+        orders && (
+          <div className="m-5">
+            <MUIDataTable
+              title={`Ordeers List(${orders?.length})`}
+              data={orders}
+              columns={orderscolumns}
+              options={orderlistOptions}
+            />
+          </div>
+        )
+      )}
+    </React.Fragment>
+  );
 }
 
-export default OrdersList
+export default OrdersList;
