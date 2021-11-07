b68e0bb99d9b84f3237e5160164d64fa2e5d4454
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-07 01:24:14 +0100

    CHANGE pizza edit route

--- routes/pizza.routes.js
+++ routes/pizza.routes.js
@@ -5,7 +5,7 @@ const pizzaController = require("../controllers/pizza.controller");
 router.get("/getPizzas", pizzaController.getPizzasList);
 router.post("/", pizzaController.registerPizza);
 router.post("/deletePizzas", pizzaController.deletePizzas);
-router.put("/:pizzaId", pizzaController.editPizza);
+router.put("/edit/:pizzaId", pizzaController.editPizza);
 router.get("/:pizzaId", pizzaController.getPizzaDetails);
 
 module.exports = router;
